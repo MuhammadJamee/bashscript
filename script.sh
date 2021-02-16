a68a92f57d3dea3a32cce4f1069910d711974b1f
jamee appiskey <jamee.appiskey@gmail.com>
2021-02-16 14:57:09 +0500

    sh file

--- script.sh
+++ script.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+apt update && apt list --upgradable && upgrade -y /
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add - /
+sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list' /
+apt update /
+apt install jenkins /
+systemctl start jenkins /
+cat /var/lib/jenkins/secrets/initialAdminPassword 
